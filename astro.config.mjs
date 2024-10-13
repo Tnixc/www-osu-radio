904b410f4ee7f151791fc8c1e1e55a19b95e2639
Tnixc <tnixxc@gmail.com>
2024-10-12 22:34:48 -0400

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  // Add your domain name here
-  site: "https://lexingtonthemes.com/",
-  integrations: [sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	// Add your domain name here
+	site: "https://lexingtonthemes.com/",
+	integrations: [sitemap()],
 });
